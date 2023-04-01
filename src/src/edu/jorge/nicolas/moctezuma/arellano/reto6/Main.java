a8f6335f5da0d48e2b38aa499c8074f3f30cf285
JorgeNMoctezuma <123213442+JorgeNMoctezuma@users.noreply.github.com>
2023-03-31 22:35:22 -0600

    reto6.2

---
+++ src/src/edu/jorge/nicolas/moctezuma/arellano/reto6/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Aprendí a usar el Menu que uso @Eryx con base al suyo profe, así bien chido
+ * Está parte manda a llamar al menu, lo que genera las opciones necesarias con información que se le dará después
+ */
+
+package edu.jorge.nicolas.moctezuma.arellano.reto6;
+import edu.jorge.nicolas.moctezuma.arellano.reto6.ui.CLI;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CLI.iniciarJuego();
+    }
+}
